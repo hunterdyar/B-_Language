09f97098efe51e11ac499842ccd5a515e8f6f69b
hunterdyar <bloop@hdyar.com>
2024-06-27 11:10:06 -0400

    extern and some tests

---
+++ BMinus/AST/PrimitiveStatements/ExternDeclaration.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace BMinus.AST.PrimitiveStatements;
+
+public class ExternDeclaration : Statement
+{
+	public Identifier[] Identifiers => _identifiers;
+	private Identifier[] _identifiers;
+
+	public ExternDeclaration(Identifier[] ids)
+	{
+		if (ids == null || ids.Length == 0)
+		{
+			throw new ArgumentException($"Null or no id's to declare?");
+		}
+
+		_identifiers = ids;
+	}
+
+	public ExternDeclaration(List<Identifier> ids)
+	{
+		if (ids == null || ids.Count == 0)
+		{
+			throw new ArgumentException($"Null or no id's to declare?");
+		}
+
+		_identifiers = ids.ToArray();
+	}
+
+	public ExternDeclaration(Identifier id)
+	{
+		_identifiers = new[] { id };
+	}
+
+	public override string ToString()
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.Append("extern ");
+		for (var i = 0; i < _identifiers.Length; i++)
+		{
+			var id = _identifiers[i];
+			sb.Append(id.Value);
+			if (i < _identifiers.Length - 1)
+			{
+				sb.Append(",");
+			}
+		}
+
+		return sb.ToString();
+	}
+}
---
+++ StarParser/Parser/Parselets/ExternDeclarationParselet.cs
@@ -0,0 +1,31 @@
+﻿using BMinus.AST;
+using BMinus.AST.PrimitiveStatements;
+using StarParser.Tokenizer;
+
+namespace StarParser.Parser.Parselets;
+
+public class ExternDeclarationParselet : IPrefixParselet
+{
+	public Statement Parse(Parser parser, Token token)
+	{
+		var variables = new List<Identifier>();
+		if (!parser.Peek(TokenType.EndStatement))
+		{
+			do
+			{
+				var idTok = parser.Consume(TokenType.Identifier);
+				variables.Add(new Identifier(idTok.Literal));
+			} while (parser.Match(TokenType.Comma));
+		}
+
+		if (variables.Count == 0)
+		{
+			throw new ParseException(
+				$"Empty extern declaration. Must declare at least one variable after {token.Literal}");
+		}
+		else
+		{
+			return new ExternDeclaration(variables);
+		}
+	}
+}

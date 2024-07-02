286ec1860d8edfde0e8570b2846cfbb0ca1fa30f
hunterdyar <bloop@hdyar.com>
2024-07-02 18:17:50 -0400

    cleanup

--- BMinusWebRuntime/wwwroot/modules/editor.mjs
+++ BMinusWebRuntime/wwwroot/modules/editor.mjs
@@ -2,16 +2,9 @@
 
 import {basicSetup} from "codemirror"
 import {EditorView, keymap} from "@codemirror/view"
-
-import {
-	cppLanguage
-}
-from "@codemirror/lang-cpp"
-
+import {cppLanguage} from "@codemirror/lang-cpp"
 import {defaultKeymap} from "@codemirror/commands"
 import {EditorState} from "@codemirror/state"
-
-
 
 const inputDiv = document.getElementById("input");
 
@@ -19,7 +12,11 @@ let startState = EditorState.create({
 	doc: "auto a;\n" +
 		"a = 1 + 2;\n" +
 		"putint(a);",
-	extensions: [basicSetup, cppLanguage, keymap.of(defaultKeymap)],
+	extensions: 
+		[	basicSetup,
+			cppLanguage,
+			keymap.of(defaultKeymap),
+		],
 	viewportMargin: Infinity
 })
 

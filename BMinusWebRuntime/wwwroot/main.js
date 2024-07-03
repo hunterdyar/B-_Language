f31a051d9904f36ee0370c91c76643a43fd37a36
hunterdyar <bloop@hdyar.com>
2024-07-03 18:30:37 -0400

    js cleanup

--- BMinusWebRuntime/wwwroot/main.js
+++ BMinusWebRuntime/wwwroot/main.js
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+// noinspection JSFileReferences
 import {dotnet} from './_framework/dotnet.js'
 import {editor} from "./js/editor.bundle.js"
 
@@ -20,7 +21,6 @@ setModuleImports('main.js', {
     onInstruction: onInstruction,
     onStack: onStack,
     onState: onState,
-    
 });
 
 const config = getConfig();
@@ -28,17 +28,17 @@ const exports = await getAssemblyExports(config.mainAssemblyName);
 exports.BMinusRuntime.Init();
 
 document.getElementById('execute').onclick = ()=>{
-    var p = editor.state.doc.toString();
+    let p = editor.state.doc.toString();
     clearRegister();
     exports.BMinusRuntime.RunProgram(p);
     RenderAST();
     
-    var data = exports.BMinusRuntime.GetGlobals();
+    // var data = exports.BMinusRuntime.GetGlobals();
 };
 
 document.getElementById('compile').onclick = () => {
         clearOutput();
-        var p = editor.state.doc.toString();
+        let p = editor.state.doc.toString();
         exports.BMinusRuntime.Compile(p);
         RenderAST();
         GetAndRenderAllInstructions();
@@ -46,15 +46,15 @@ document.getElementById('compile').onclick = () => {
 
 document.getElementById('step').onclick = ()=>{
     //if state is ready, firstStep
-    var s = exports.BMinusRuntime.GetState();
-    if(s == 5 || s == 4 || s == 3){//uninitiazed, complete, error
-        var p = editor.state.doc.toString();
+    let s = exports.BMinusRuntime.GetState();
+    if(s === 5 || s === 4 || s === 3){//uninitiazed, complete, error
+        let p = editor.state.doc.toString();
         clearOutput();
         exports.BMinusRuntime.Compile(p);
         RenderAST();
         exports.BMinusRuntime.Step();
 
-    }else if(s == 2 || s == 0){
+    }else if(s === 2 || s === 0){
     //if state is stepping, or compiled/ready. step
         exports.BMinusRuntime.Step();
     }else{
@@ -74,7 +74,7 @@ function onOutput(outputText){
 const stateChip = document.getElementById('vmState');
 onState(5);//assume uinitialized to start.
 function onState(state){
-    var text = "unknown";
+    let text = "unknown";
     //see VMState.cs enum
     switch(state){
         case 0:
@@ -108,8 +108,9 @@ const reg = [
 ];
 function onRegister(data){
 //highlight the registers that changed on that frame.
-    for (var r=0;r<reg.length;r++){
-        var changed = data[r] != reg[r].innerText;
+    for (let r=0;r<reg.length;r++){
+        // noinspection EqualityComparisonWithCoercionJS
+        let changed = data[r] != reg[r].innerText;
         reg[r].innerText = data[r];
         if(!changed) {
             if (reg[r].classList.contains('changed')) {
@@ -124,7 +125,7 @@ function onRegister(data){
 }
 
 function clearRegister(){
-    for (var r = 0; r < reg.length; r++) {
+    for (let r = 0; r < reg.length; r++) {
         reg[r].innerText = "0";
         if (reg[r].classList.contains('changed')) {
             reg[r].classList.remove('changed');
@@ -153,9 +154,9 @@ function onInstruction(ins){
     // instructionOutput[2].parentElement.hidden = ins[2].length===0;
     instructionOutput[3].innerText = getTooltip(ins[0]);
     //i hate that we parse this. maybe two function calls that get sent back to back. "on new instruction" (3 strings) and "on new instruction location" (3 ints)
-    let astID = Number.parseInt(ins[3]);
+   // let astID = Number.parseInt(ins[3]);
     let frameProto = Number.parseInt(ins[4]);
-    let insloc = Number.parseInt(ins[5]);
+    //let insloc = Number.parseInt(ins[5]);
     
     if(ast != null){
         if (ast.classList.contains('changed')) {
@@ -212,7 +213,7 @@ const stacksize = document.getElementById("stackSize");
 function onStack(ins, opCount){
     stacksize.innerText = opCount;
     stacklist.innerHTML = "";
-    for(var i = 0;i<ins.length;i++){
+    for(let i = 0;i<ins.length;i++){
         stacklist.innerHTML += "<tr><td class=\"min\">"+i+"</td><td>"+ins[i]+"</td></tr>";
     }
 }
@@ -220,8 +221,8 @@ function onStack(ins, opCount){
 const tree = document.getElementById("syntaxTree");
 //todo: call from a "onCompiled"
 function RenderAST(){
-    var treeNode = exports.BMinusRuntime.GetAST();
-    var n = JSON.parse(treeNode);
+    let treeNode = exports.BMinusRuntime.GetAST();
+    let n = JSON.parse(treeNode);
     tree.innerHTML = "";
     RenderTreeNode(tree,n);
 }
@@ -238,7 +239,7 @@ function RenderTreeNode(parentNode, element){
         }
         return;
     }
-    var detailsNode = document.createElement("details");
+    let detailsNode = document.createElement("details");
     detailsNode.open = true;
     parentNode.append(detailsNode);
     
@@ -246,7 +247,7 @@ function RenderTreeNode(parentNode, element){
     detailsNode.append(name);
     name.id = "ast-" + element["id"];
 
-    var contentList = document.createElement("ul");
+    let contentList = document.createElement("ul");
     detailsNode.append(contentList);
     
     //set name
@@ -257,8 +258,8 @@ function RenderTreeNode(parentNode, element){
     }
     
     //create children
-    for(var i = 0;i<element.children.length;i++){
-        var childItem = document.createElement("li");
+    for(let i = 0;i<element.children.length;i++){
+        let childItem = document.createElement("li");
         contentList.append(childItem);
         RenderTreeNode(childItem,element.children[i]);
     }
@@ -290,41 +291,40 @@ function GetAndRenderAllInstructions(){
     while (fullInstructionList.lastChild.id !== "tabLinks") {
         fullInstructionList.removeChild(fullInstructionList.lastChild);
     }
-    var numberFrames = exports.BMinusRuntime.GetFrameCount();
-    var firstlink;
+    let numberFrames = exports.BMinusRuntime.GetFrameCount();
+    let firstlink;
 
     for (let curFrame = 0;curFrame<numberFrames;curFrame++) {
 
-        var i = curFrame;
-        var f = exports.BMinusRuntime.GetInstructions(curFrame);
-        var link = document.createElement("a");
+        const i = curFrame;
+        let f = exports.BMinusRuntime.GetInstructions(curFrame);
+        let link = document.createElement("a");
         link.id = "frame-link-" + i.toString();
         link.innerText = i === 0 ? "Instructions" : i.toString();
         if (curFrame === 0) {
             firstlink = link;
         }
         //todo: fix closure, make a function and make a closure that has the id.
-        const id = i;
-        link.addEventListener("click", () => setActiveInstructionList(id),false);
+        link.addEventListener("click", () => setActiveInstructionList(i),false);
         
         instructionTabs.append(link);
-        var pageContainer = document.createElement("div");
+        const pageContainer = document.createElement("div");
         pageContainer.classList.add("page");
         pageContainer.classList.add("scroll");
         pageContainer.classList.add("small-height");
         pageContainer.id = "frame-" + i.toString();
         fullInstructionList.append(pageContainer);
 
-        var table = document.createElement("table");
+        const table = document.createElement("table");
         table.classList.add("border");
         table.classList.add("no-space");
         pageContainer.append(table);
         table.innerHTML = "<thead><th>#</th><th>Instruction</th ><th>Op A</th><th>Op B</th></thead>";
-        var tableBody = document.createElement("tbody");
+        const tableBody = document.createElement("tbody");
         table.append(tableBody);
         for (let j = 0; j < f.length; j += 5) {
             //ins.Op.ToString(), a, b, ins.ASTNodeID.ToString()
-            var row = document.createElement("tr");
+            const row = document.createElement("tr");
             row.classList.add("min");
             row.id = "ins-" + i.toString() + "-" + (j / 5).toString();
             row.innerHTML = "<td>"+(j/5)+" </td><td>" + f[j] + "</td><td>" + f[j + 1] + "</td><td>" + f[j + 2] + "</td>";
